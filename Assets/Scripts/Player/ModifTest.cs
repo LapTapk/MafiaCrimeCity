9024ec09f10b0c309fae8553d7d1a7561dd6d602
Lap_Tapk <fret4657@gmail.com>
2021-11-30 00:10:15 +0300

    Removed temporary develop files

--- Assets/Scripts/Player/ModifTest.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ModifTest : MonoBehaviour
-{
-    private void Start()
-    {
-        PlayerInstance.Instance.gameObject.GetComponent<PlayerMovement>().Modifier.Add(() => 0.5f, 4);
-    }    
-}
